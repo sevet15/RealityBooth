edbddcee7f32fc840684c63cf76315768e79eee9
sevet15 <stvalentino999@gmail.com>
2026-08-19 11:31:14 +0700

    setup project

---
+++ RealityBooth/RealityBooth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RealityBooth
+//
+//  Created by Steven Valentino on 19/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RealityBooth/RealityBooth/RealityBoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealityBoothApp.swift
+//  RealityBooth
+//
+//  Created by Steven Valentino on 19/08/26.
+//
+
+import SwiftUI
+
+@main
+struct RealityBoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
